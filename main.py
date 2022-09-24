db5d575bb8b22e78b5c5a03632929ada985b291c
simonuks221 <simonasriauka@gmail.com>
2022-09-24 18:55:07 +0300

    Main animacijos + tomo ukraina + lietuva

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!./venv/bin/python
 import time
 import numpy as np
-#import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import asyncio
 from time import *
 from serial import Serial
@@ -16,30 +16,29 @@ from head import *
 from joint import *
 from simonoAnimacijos import *
 
-#from audio import AudioFile, AudioMic, smooth_fft_values
+# from audio import AudioFile, AudioMic, smooth_fft_values
 # from control_socket import ControlSocket
 
 # from examples import argb_strip_example, argb_fft_example, argb_fft_pygame_example
 
 # soc = ControlSocket("My awesome installation", "http://localhost:8080")
 
-s = Serial('/dev/ttyUSB0', 460800)
-#s = Serial('COM7', 460800)
+#s = Serial('/dev/ttyUSB0', 460800)
+s = Serial('COM7', 460800)
 s.timeout = 0.01
 
 
 head_init(s, allPixels)
-#jointai.append(SegmentJoint([allEdges[0], allEdges[1]]))
+# jointai.append(SegmentJoint([allEdges[0], allEdges[1]]))
 
 head_draw_all(s, 0, 0, 0)
 head_clear_all(s)
 update_all_argb(s)
 sleep(0.5)
 
-song = AudioFile('sound2.wav')
-song.play()
+#song = AudioFile('sound2.wav')
+# song.play()
 while True:
-    '''
     r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
@@ -48,13 +47,21 @@ while True:
     full = random.randint(0, 3)
     print(duration)
     blink_EYEMou(s, blinks, r, g, b)
-    if (blinks == 1):
+    if (full == 1):
         head_draw_all(s, r, g, b)
         update_all_argb(s)
     else:
         draw_all_exc_EyeMou(s, duration, r, g, b)
 
+    full - random.randint(0, 4)
+    if full == 0:
+        FlagsUkraine(s)
+        sleep(0.1)
+    elif full == 1:
+        FlagsLTU(s)
+        sleep(0.1)
+
     gen_rand_stripes(s, 5, r, g, b)
     sleep(0.1)
-    '''
-    SongAnimations(s, song)
+
+    #SongAnimations(s, song)
